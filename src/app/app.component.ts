303a707bff158adbbd2c0d2d11164b34d4f6493a
alislahprahim <alislahiprahim99@gmail.com>
2024-01-05 15:50:45 +0200

    tailwind

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
@@ -10,5 +10,24 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'test-tailwind';
+  mobileMenu: boolean = false;
+  constructor(private elementRef: ElementRef) { }
+
+  ngAfterViewInit(): void {
+    let allDropdowns: any[] = [];
+    allDropdowns = this.elementRef.nativeElement.querySelectorAll('.dropdown');
+    Array.from(allDropdowns).forEach(d => {
+      let dropdownList = d.querySelector('.dropdown-list')
+      d.addEventListener('click', (event: any) => {
+        console.log('event', event, dropdownList);
+        if (dropdownList.classList.contains('hidden')) {
+          dropdownList.classList.remove('hidden')
+          dropdownList.classList.add('block')
+        } else {
+          dropdownList.classList.add('hidden');
+          dropdownList.classList.remove('block');
+        }
+      })
+    })
+  }
 }
